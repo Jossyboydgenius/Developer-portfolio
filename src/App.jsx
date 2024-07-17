715c1de22bbd77919815b5a3fecbc3b6da7f6a76
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-07-17 16:45:34 +0000

    refactor: Add modular components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+import About from "./Components/About"
+import Skill from "./Components/Skill"
+import Services from "./Components/Services"
+import Projects from "./Components/Projects"
+import Testimonial  from "./Components/Testimonial"
+import Contact from "./Components/Contact"
+import Footer from "./Components/Footer"
+
 function App() {
 
   return (
     <>
-     <h1 style={{color:'white'}}>Thanks My Subscribers</h1>
+     <Navbar/>
+     <Hero/>
+     <About/>
+     <Skill/>
+     <Services/>
+     <Projects/>
+     <Testimonial/>
+     <Contact/>
+     <Footer/>
     </>
   )
 }
