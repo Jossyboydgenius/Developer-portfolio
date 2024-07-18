807701b71ded80db5e3d2b989e8ccbc7a3799969
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-07-18 17:45:27 +0000

    feat: Add Logo component

---
+++ src/Commons/Logo/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Logo.css"
+import { FaLaptopCode } from 'react-icons/fa'
+
+const Logo = () => {
+  return (
+    <div className='logo'>
+      <FaLaptopCode className='icon'/>
+        <h1>Portfolio</h1>
+    </div>
+  )
+}
+
+export default Logo
