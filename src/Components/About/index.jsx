e0b342231f27499cb52158b6c671f0cec03b72fe
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-07-20 04:54:46 +0000

    chore: Update About component with responsive layout and new content

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import "./About.css"
+import { whyChooseMe } from '../../sources'
 
 const About = () => {
   return (
-    <div>
+    <section id='about'>
+      <div className="wrapper">
+        <div className="section-header">
+          <h1 className="heading-1">
+            <span className="gradient-text">About Me</span>
+          </h1>
+          <h4 className="sub-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At distinctio facere omnis alias laborum recusandae magnam harum velit cupiditate,
+            veritatis exercitationem blanditiis quas quisquam, est obcaecati. Ullam adipisci hic voluptate.
+          </h4>
+        </div>
+        <div className="group">
+          {
+            whyChooseMe.map((list, index) => (
+              <div className='flex-center group-item' key={index}>
+                <div className="flex-center icon-wrapper">
+                {list.icon}
+                </div>
+                <h4 className="title">{list.title}</h4>
+              </div>
+            ))
+          }
+        </div>
+      </div>
       About
-    </div>
+    </section>
   )
 }
 
