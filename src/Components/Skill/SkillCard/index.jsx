1e90d78998bdf740198335f01d49389dc9370d66
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-07-20 05:29:42 +0000

    feat: Add SkillCard component for displaying skills

---
+++ src/Components/Skill/SkillCard/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./SkillCard.css"
+import { BsPatchCheckFill } from "react-icons/bs"
+
+const SkillCard = ({data, title}) => {
+  return (
+    <div className='skill-card'>
+        <h3 className="title gradient-text">{title}</h3>
+        <div className="skill-content">
+            {
+            data.map((list, index) => (
+                <div className="skill-details" key={index}>
+                <BsPatchCheckFill className="skill-icon"/>
+                <div className="details">
+                    <h4 className="skill-name">{list.skill}</h4>
+                    <small className="muted skill-level">{list.level}</small>
+                </div>
+             </div>
+            ))
+        }
+    </div>
+    </div>
+  )
+}
+
+export default SkillCard
