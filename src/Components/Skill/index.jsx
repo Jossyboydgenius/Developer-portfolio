cb98336a562b6024d1c3a80be6169bcef8da86f1
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-07-20 05:29:33 +0000

    chore: Refactor Skill component for improved readability and maintainability

--- src/Components/Skill/index.jsx
+++ src/Components/Skill/index.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import "./Skill.css"
+import { skills } from '../../sources'
+import SkillCard from './SkillCard'
 
 const Skill = () => {
   return (
-    <div>
+    <section id='skill'>
+      <div className="wrapper">
+        <div className="wrapper">
+        <div className="section-header">
+          <h1 className="heading-1">
+            <span className="gradient-text">My Skills</span>
+          </h1>
+        </div>
+        <div className="skills-container">
+          {
+            skills.map((list, index) => (
+              <SkillCard 
+              {...list}
+              key={index}/>
+            ))
+          }
+        </div>
+        </div>
+      </div>
       Skill
-    </div>
+    </section>
   )
 }
 
