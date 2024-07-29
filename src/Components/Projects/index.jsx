d38edcdddfe628765fd9b8b4e383397cb7b6b7f2
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-07-29 22:18:26 +0000

    refactor: Add lazy loading for images in Projects component

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Projects.css"
+import { projects } from '../../sources'
+import ProjectCard from './ProjectCard'
 
 const Projects = () => {
+  const [activeProjects, setActiveProjects] = useState(projects)
   return (
-    <div>
-      Projects
-    </div>
+    <section id='projects'>
+      <div className="wrapper">
+      <div className="section-heade">
+        <h1 className="heading-1">
+          <span className="gradient-text">Projects</span>
+        </h1>
+      </div>
+      <div className="projects-container">
+        {
+          activeProjects.map((project, index) => (
+            <ProjectCard 
+            {...project}
+            key={index} project={project}/>
+          ))
+        }
+      </div>
+      </div>
+    </section>
   )
 }
 
