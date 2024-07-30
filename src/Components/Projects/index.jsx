d14e83d956ac657d400efdc7976335043e51fb26
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-07-30 23:18:44 +0000

    refactor: Add ProjectNavigation component to Projects page

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -2,17 +2,28 @@ import React, {useState} from 'react'
 import "./Projects.css"
 import { projects } from '../../sources'
 import ProjectCard from './ProjectCard'
+import ProjectNavigation from './ProjectNavigation'
 
 const Projects = () => {
   const [activeProjects, setActiveProjects] = useState(projects)
+  const getTabs = () => {
+    const tabs = ["All"];
+    projects.map((item) => {
+      if (!tabs.includes(item.category)) {
+        tabs.push(item.category);
+      }
+    });
+      return tabs;
+    }
   return (
     <section id='projects'>
       <div className="wrapper">
-      <div className="section-heade">
+      <div className="section-header">
         <h1 className="heading-1">
           <span className="gradient-text">Projects</span>
         </h1>
       </div>
+      <ProjectNavigation tabs={getTabs()}/>
       <div className="projects-container">
         {
           activeProjects.map((project, index) => (
