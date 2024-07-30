f1b21c2633c6eac99cef644a81ffb1ba73addbfa
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-07-30 23:19:02 +0000

    feat: Add ProjectNavigation component for project navigation

---
+++ src/Components/Projects/ProjectNavigation/index.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from 'react'
+import "./ProjectNavigation.css"
+
+const ProjectNavigation = ({
+  tabs,
+  onChange,
+}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const [indicatorWidth, setIndicatorWidth] = useState(0);
+  const itemsEls = useRef(new Array());
+  const sumArray = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+  useEffect(() => {
+    const prevEl = itemsEls.current.filter((_, index) => index < activeIndex);
+    setOffset(
+      sumArray(prevEl.map(item => item.offsetWidth))
+    )
+    setIndicatorWidth(itemsEls.current[activeIndex].offsetWidth);
+  }, [activeIndex]);
+  return (
+    <nav className='project-navigation'>
+      {
+        tabs.map((tab, index) => (
+          <button 
+            ref={el => itemsEls.current[index] = el}
+            onClick={() => {
+              setActiveIndex(index);
+              onChange(tab);
+            }}
+            key={index}
+            >
+              {tab}
+          </button>
+        ))
+      }
+      <span 
+      className="indicator"
+      style={{
+        left:`${offset}px`,
+        right:`${indicatorWidth}px`,
+      }}
+      />
+    </nav>
+  )
+}
+
+export default ProjectNavigation
