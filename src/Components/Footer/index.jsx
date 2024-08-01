f5386f93999d53c5c5d7f36559ca4138ffc0c015
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-01 17:42:56 +0000

    refactor: Update Footer component to include logo, social handles, and footer links

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import "./Footer.css"
+import Logo from '../../Commons/Logo'
+import SocialHandles from '../../Commons/SocialHandles'
+import { footer } from '../../sources'
+import { Link } from 'react-scroll'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer id='footer'>
+      <div className="wrapper">
+        <div className="column">
+          <Logo/>
+          <SocialHandles/>
+        </div>
+        {
+          footer.map((list, index) => (
+            <div className="column" key={index}>
+              <h3 className="muted title">{list.title}</h3>
+              {
+                list.routes.map((route, index) => (
+                  <Link to={route.id || ""} smooth={true} className="route" key={index}>
+                  {route.name}
+                  </Link>
+                ))
+              }
+              </div>
+          ))
+        }
+      </div>
+      <div className="flex-center copyright">
+        <h4>Copyright &copy; All right reserved -| 2024</h4>
+        <p className="muted">Built with love by Awe Joseph A.K.A TechApostle</p>
+      </div>
+    </footer>
   )
 }
 
 export default Footer
+
