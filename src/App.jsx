9cc5f01162fb3540381f469ab0549909672f1bf3
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-01 05:46:50 +0000

    refactor: Rename Testimonial component to Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from "./Components/About"
 import Skill from "./Components/Skill"
 import Services from "./Components/Services"
 import Projects from "./Components/Projects"
-import Testimonial  from "./Components/Testimonial"
+import Testimonial  from "./Components/Testimonials"
 import Contact from "./Components/Contact"
 import Footer from "./Components/Footer"
 
