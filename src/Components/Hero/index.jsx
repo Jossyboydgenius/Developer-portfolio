bab85928974bde45a0d7602d42653a92cf85741d
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-01 19:50:54 +0000

    style: Update Hero component with AOS animations and responsive button layout

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -9,24 +9,24 @@ const Hero = () => {
     <section id='hero'>
       <div className="wrapper info-container">
         <div className="column">
-          <h3 className='sub-title'>
+          <h3 className='sub-title' data-aos='fade-right'>
             Hello, I'm <span className='primary'>Awe Joseph</span>
           </h3>
-          <h1 className="heading-1">
+          <h1 className="heading-1" data-aos='fade-up'>
             A <span className="gradient-text">Fullstack</span> Software Engineer
           </h1>
-          <p className="muted">
+          <p className="muted" data-aos='fade-up' data-aos-delay='300'>
           {/* // TODO: Change this text to something more personal */}
             Lorem ipsum dolor, sit amet consectetur adipisicing elit.
             Quaerat consectetur sapiente, beatae deserunt reprehenderit eaque hic, ullam distinctio inventore nemo,
             ea nam facere voluptas maxime laborum. Ipsum, vel? Quo, behjatae!
           </p>
           <div className="flex-center buttons-wrapper">
-            <Link to='services' smooth={true} className='btn primary'>Learn more</Link>
-            <Link to='contact' smooth={true} className='btn secondary'>Get started</Link>
+            <Link to='services' smooth={true} className='btn primary' data-aos='fade-left' data-aos-delay='1300' data-aos-offset='50'>Learn more</Link>
+            <Link to='contact' smooth={true} className='btn secondary' data-aos='fade-left' data-aos-delay='1600' data-aos-offset='50'>Get started</Link>
           </div>
         </div>
-        <div className="column hero-image">
+        <div className="column hero-image" data-aos='fade-left' data-aos-delay='300'>
           <img src={me} alt="" />
         </div>
       </div>
