03f0fc9531e4db2e38499f959979646fdbf1eadd
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-01 19:50:45 +0000

    refactor: Initialize Aos library for smooth animations on page load

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,17 @@ import Projects from "./Components/Projects"
 import Testimonial  from "./Components/Testimonials"
 import Contact from "./Components/Contact"
 import Footer from "./Components/Footer"
+import { useEffect } from "react"
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      duration: 500,
+    easing: 'ease-in-out-sine',
+  })
+},[])
 
   return (
     <>
