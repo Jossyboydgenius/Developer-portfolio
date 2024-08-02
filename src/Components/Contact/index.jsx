ed6bdbb149ef304fe67c5031ad067ea4fd0c1453
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-02 06:32:02 +0000

    refactor: Update Contact component with AOS animations and responsive layout

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -4,12 +4,12 @@ import { contactOptions } from '../../sources'
 
 const Contact = () => {
   return (
-    <section id='contact'>
+    <section id='contact' data-aos='fade-zoom-in'>
       <div className="wrapper">
         <div className="contact-options">
           {
             contactOptions.map((option, index) => (
-              <div className="flex-center option" key={index}>
+              <div className="flex-center option" data-aos='fade-right' key={index}>
                 <div className="flex-center icon-wrapper">{option.icon}</div>
                 <h4 className="muted">{option.title}</h4>
                 <h3 className="value">{option.value}</h3>
@@ -17,7 +17,7 @@ const Contact = () => {
             ))
           }
         </div>
-        <div className="contact-form">
+        <div className="contact-form" data-aos='fade-left'>
           <div className="top">
             <h1 className="title">
               <span className="gradient-text">Join forces with me!</span>
