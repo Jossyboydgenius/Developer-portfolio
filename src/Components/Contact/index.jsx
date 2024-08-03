79b15774128cd94961a372dc2b053d3e8ee74403
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-03 11:05:21 +0000

    chore: Integrated formspree to handle message submission.

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -20,27 +20,29 @@ const Contact = () => {
         <div className="contact-form" data-aos='fade-left'>
           <div className="top">
             <h1 className="title">
-              <span className="gradient-text">Join forces with me!</span>
+              <span className="gradient-text">Let's Collaborate!</span>
             </h1>
             <p className="muted">
-              I build with precision and passion, creating robust and scalable applications, web projects that stands out.
-              I am always open to new opportunities and collaborations. Feel free to reach out to me for any enquiry.
-              </p>
+            Always open to new opportunities and collaborations, I invite you to connect with me for 
+            any inquiries or project discussions.
+            </p>
           </div>
-          <div className="middle">
-            <div className="flex row">
-              <input type="text" placeholder='First name' name='firstname' className='control'/>
-              <input type="text" placeholder='Last name' name='lastname' className='control'/>
+          <form action="https://formspree.io/f/xyzgwlky" method="POST">
+            <div className="middle">
+              <div className="flex row">
+                <input type="text" placeholder='First name' name='firstname' className='control' required />
+                <input type="text" placeholder='Last name' name='lastname' className='control' required />
+              </div>
+              <div className="flex row">
+                <input type="email" placeholder='Email address' name='email' className='control' required />
+                <input type="tel" placeholder='Phone number' name='phone' className='control' />
+              </div>
+              <textarea name="message" cols={30} rows={10} placeholder='Message' className='control' required></textarea>
             </div>
-            <div className="flex row">
-              <input type="email" placeholder='Email address' name='email' className='control'/>
-              <input type="tel" placeholder='Phone number' name='phone' className='control'/>
+            <div className="flex-center bottom">
+              <button type='submit' className='btn primary'>Send Now</button>
             </div>
-            <textarea name="message" cols={30} rows={10} placeholder='Message' className='control'></textarea>
-          </div>
-          <div className="flex-center bottom">
-            <button className='btn primary'>Send Now</button>
-          </div>
+          </form>
         </div>
       </div>
     </section>
